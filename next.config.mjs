603c60dc0269e5347948f2b7496757b1d295197d
Yassine Ghorbel <yassine.ghorbel@supcom.tn>
2025-05-27 19:13:19 +0100

    fix next file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'imgur.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.loca.lt',
+      },
+    ],
+  },
+  experimental: {},
+  serverExternalPackages: ['@tensorflow/tfjs-node'], // ✅ Updated here
+};
 
 export default nextConfig;
