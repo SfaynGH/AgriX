99862552bb3d845c09efb41379697569274f3086
Yassine Ghorbel <yassine.ghorbel@supcom.tn>
2025-05-27 19:29:56 +0100

    fix icons

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,3 +2,9 @@ export type SoilMoistureMetric = {
   date: string;
   moisture: number;
 };
+
+// src/types/types.ts
+
+export type IconSvgProps = React.SVGProps<SVGSVGElement> & {
+  size?: number;
+};
